3c7ab9163b024fd3531d8ae475a68b97e954584a
King-On Yeung <koyeung@gmail.com>
2016-04-11 21:19:23 +0800

    fix compile problem on project depending on this one

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule StreamX.Mixfile do
 
   def project do
     [app: :stream_x,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -11,7 +11,7 @@ defmodule StreamX.Mixfile do
      package: package,
      deps: deps,
      source_url: "https://github.com/koyeung/stream_x",
-     docs: docs,
+     docs: &docs/0,
     ]
   end
 
