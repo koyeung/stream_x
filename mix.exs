b7e5b6b18dc82c149dfe367cf6dfe759d1c147f0
King-On Yeung <koyeung@gmail.com>
2016-04-11 21:24:16 +0800

    revise version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule StreamX.Mixfile do
 
   def project do
     [app: :stream_x,
-     version: "0.0.2",
+     version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
